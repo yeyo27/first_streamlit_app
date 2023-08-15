c69f0c5dae202c797635548dc487d2a801800be9
Sergio Mesa Martín <114915232+yeyo27@users.noreply.github.com>
2023-08-16 00:26:03 +0200

    Update streamlit_app.py
    Scaped single quotes from title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 fruit_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 
