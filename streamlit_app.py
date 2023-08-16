e1e6e1ffff25c82babf88e851a39b66a50c2b73a
Sergio Mesa Martín <114915232+yeyo27@users.noreply.github.com>
2023-08-16 10:10:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,11 @@ fruit_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fr
 streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
-
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect('Pick some fruits:', fruits_df.index)
 streamlit.dataframe(fruit_df)
