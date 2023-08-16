1d25072fb1e7915b497068171957d11cb4bc12fc
Sergio Mesa Martín <114915232+yeyo27@users.noreply.github.com>
2023-08-16 13:11:53 +0200

    Update streamlit_app.py
    Corrected typo in line 24

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,5 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalize)
