bbb52409f50ad16533aa2a4e463ed1ae57eb58c5
Sergio Mesa Martín <114915232+yeyo27@users.noreply.github.com>
2023-08-16 10:11:42 +0200

    Update streamlit_app.py
    Added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect('Pick some fruits:', fruits_df.index)
+streamlit.multiselect('Pick some fruits:', list(fruits_df.index))
 streamlit.dataframe(fruit_df)
